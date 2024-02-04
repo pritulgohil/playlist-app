bd638b911400b0070a281d584363298a77357410
pritulgohil <34166907+pritulgohil@users.noreply.github.com>
2024-02-03 21:17:57 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const cacheName = 'cacheAssets9'
+
+self.addEventListener('install',(event) => {
+    console.log('Install!', event);
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(cacheName)
+    .then((cache) => {
+       console.log('Cache:', cache)
+       cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/Assets/logo.png'
+       ]);
+    })
+    .catch((error) => {
+        console.log('Cache failed:',error)
+    })
+    );
+}); 
+
+self.addEventListener('activate', (event) => {
+    //console.log('Activate:', event);
+    event.waitUntil(clients.claim());
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => {
+                console.log('Cache Names', cacheNames);
+                cacheNames.forEach((item) => {
+                    console.log(item);
+                    if(item !== cacheName){
+                        caches.delete(item);
+                    }
+                })
+            })
+    )
+})
+
+self.addEventListener('fetch',(event) => {
+    event.respondWith(
+        caches.open(cacheName)
+            .then((cache) => {
+                return cache.match(event.request)
+                    .then((response) => {
+                        if(response) {
+                            return response;
+                        } else{
+                            return fetch(event.request);
+                        }
+                    })
+            })
+    )
+})
