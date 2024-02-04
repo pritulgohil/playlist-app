64a4d3243f105f57d757a0fa8bb8fb75b74c060b
pritulgohil <34166907+pritulgohil@users.noreply.github.com>
2024-02-03 21:21:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('service-worker.js',{scope:'/'})
+    .then((registration) => {
+      console.log('Register Success:', registration);
+    })
+    .catch((error) => {
+      console.log('Register Failed:', error);
+    });
+} else { 
+  console.log('Service Workers are not supported');
+}
+
+
+
 let titleField = document.getElementById('title')
 let artistField = document.getElementById('artist')
 let titleContainer = document.getElementById('title-container')
@@ -14,27 +28,28 @@ artistField.addEventListener('input',function(){
     artistFieldValue = artistField.value
 })
 
-
-
 addButton.addEventListener('click', function () {
-    if (titleFieldValue.trim() === '' || artistFieldValue.trim() === '') {
-      // Log "Pritul" to the console when titleFieldValue is empty
+    if(titleFieldValue.trim() === '' && artistFieldValue.trim() === ''){
       errorDisplay.style.display = "block"
+      errorDisplay.innerHTML = "Input Title & Artist Name"
+      console.log(titleFieldValue)
+    }
+    else if (titleFieldValue.trim() === '') {
+      errorDisplay.style.display = "block"
+      errorDisplay.innerHTML = "Input Title Name!"
+    } else if (artistFieldValue.trim() === ''){
+      errorDisplay.style.display = "block"
+      errorDisplay.innerHTML = "Input Artist Name!"
     } else {
-      // Create new elements and append them to the titleContainer
       errorDisplay.style.display = "none"
       let newPara1 = document.createElement('p');
       let newPara2 = document.createElement('p');
       let divider = document.createElement('hr');
-  
       newPara1.classList.add('bold');
       newPara1.innerHTML = titleFieldValue;
       titleContainer.appendChild(newPara1);
-  
       newPara2.innerHTML = artistFieldValue;
       titleContainer.appendChild(newPara2);
-  
       titleContainer.appendChild(divider);
     }
   });
-
